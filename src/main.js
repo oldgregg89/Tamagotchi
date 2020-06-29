b6808583ae9b2aa90ae147a8f3a6f63439409b71
Ian Gregg <iangregg188@gmail.com>
2020-06-29 15:32:33 -0700

    Worked even more on the UI logic health bars.
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- src/main.js
+++ src/main.js
@@ -10,54 +10,55 @@ $(document).ready(function() {
     $("#showName").text($("#petName").val());
     $("#game").show();
     $("form").hide();
+
+    let newPet = new Pet(Math.floor((Math.random() * (100 - 50)) + 50), Math.floor((Math.random() * (100 - 50)) + 50), Math.floor((Math.random() * (100 - 50)) + 50));
+    newPet.degredation();
     $("#foodBar").text(newPet.food);
     $("#playBar").text(newPet.happiness);
     $("#napBar").text(newPet.sleep);
-  });
 
-  let newPet = new Pet(Math.floor((Math.random() * (100 - 50)) + 50), Math.floor((Math.random() * (100 - 50)) + 50), Math.floor((Math.random() * (100 - 50)) + 50));
-  newPet.degredation();
+    $("#feedPet").click(function() {
+      newPet.feedPet();
+      setInterval(() => {
+        $("#sleeping").hide();
+        $("#dancing").hide();
+        $("#havingfun").hide();
+        $("#eating").show();
+      },2000);
+    });
 
-  $("#feedPet").click(function() {
-    newPet.feedPet();
-    setInterval(() => {
-      $("#sleeping").hide();
-      $("#dancing").hide();
-      $("#havingfun").hide();
-      $("#eating").show();
-    },2000);
-  });
+    $("#playWithPet").click(function() {
+      newPet.playWithPet();
+      setInterval(() => {
+        $("#sleeping").hide();
+        $("#dancing").hide();
+        $("#havingfun").show();
+        $("#eating").hide();
+      },2000);
+    });
 
-  $("#playWithPet").click(function() {
-    newPet.playWithPet();
-    setInterval(() => {
-      $("#sleeping").hide();
-      $("#dancing").hide();
-      $("#havingfun").show();
-      $("#eating").hide();
-    },2000);
-  });
+    $("#restPet").click(function() {
+      newPet.restPet();
+      setInterval(() => {
+        $("#sleeping").show();
+        $("#dancing").hide();
+        $("#havingfun").hide();
+        $("#eating").hide();
+      },2000);
+    });
 
-  $("#restPet").click(function() {
-    newPet.restPet();
-    setInterval(() => {
-      $("#sleeping").show();
-      $("#dancing").hide();
-      $("#havingfun").hide();
-      $("#eating").hide();
-    },2000);
-  });
+    setInterval(function() {
+      $("#foodBar").html(newPet.food);
+      $("#playBar").html(newPet.happiness);
+      $("#napBar").html(newPet.sleep);
+      $("#foodBar").css("width", newPet.food + "%");
+      $("#playBar").css("width", newPet.happiness + "%");
+      $("#napBar").css("width", newPet.sleep + "%");
+    });
 
-  let reduce = setInterval(function() {
-    $("#foodBar").html(newPet.food);
-    $("#playBar").html(newPet.happiness);
-    $("#napBar").html(newPet.sleep);
-    $("#foodBar").css("width", newPet.food + "%");
-    $("#playBar").css("width", newPet.happiness + "%");
-    $("#napBar").css("width", newPet.sleep + "%");
-  });
-
-  $("#tryAgain").click(function() {
-    location.reload();
+    $("#tryAgain").click(function() {
+      location.reload();
+      return false;
+    });
   });
 });
