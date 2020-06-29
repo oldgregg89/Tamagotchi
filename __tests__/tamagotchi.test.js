ae031972a4f76cc20adc91ccda13d7122e01dc65
Ian Gregg <iangregg188@gmail.com>
2020-06-29 10:07:33 -0700

    Properties passed the red grenn test.
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- __tests__/tamagotchi.test.js
+++ __tests__/tamagotchi.test.js
@@ -8,8 +8,8 @@ describe('Pet', () => {
   });
 
   test('should verify that the Pet constructor with the properties of food, sleep, and happiness', () => {
-    expect(newPet.food).toEqual(9);
-    expect(newPet.sleep).toEqual(9);
-    expect(newPet.happiness).toEqual(9);
+    expect(newPet.food).toEqual(10);
+    expect(newPet.sleep).toEqual(10);
+    expect(newPet.happiness).toEqual(10);
   });
 });
