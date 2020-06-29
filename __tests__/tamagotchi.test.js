e7b7100d0fe83dd0a27ee40ff3fc80c47ed0e28c
Ian Gregg <iangregg188@gmail.com>
2020-06-29 11:06:02 -0700

    Pet status failed the red green test.
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- __tests__/tamagotchi.test.js
+++ __tests__/tamagotchi.test.js
@@ -5,32 +5,32 @@ describe('Pet', () => {
   jest.useFakeTimers();
   
   beforeEach(function () {
-  newPet = new Pet (10, 10, 10);
+  newPet = new Pet (9, 9, 9);
   newPet.degredation();
   });
 
   test('should verify that the Pet constructor with the properties of food, sleep, and happiness', () => {
-    expect(newPet.food).toEqual(10);
-    expect(newPet.sleep).toEqual(10);
-    expect(newPet.happiness).toEqual(10);
+    expect(newPet.food).toEqual(9);
+    expect(newPet.sleep).toEqual(9);
+    expect(newPet.happiness).toEqual(9);
   });
 
   test('Should show the feed property works with raising the lvl of the tamagotchi', () => {
-    expect(newPet.feedPet()).toEqual(11);
+    expect(newPet.feedPet()).toEqual(10);
   });
 
   test('Should verify they are losing sleep, happiness, food in intervals', () => {
     jest.advanceTimersByTime(20000);
-    expect(newPet.food).toEqual(9);
-    expect(newPet.happiness).toEqual(9);
-    expect(newPet.sleep).toEqual(9);
+    expect(newPet.food).toEqual(8);
+    expect(newPet.happiness).toEqual(8);
+    expect(newPet.sleep).toEqual(8);
   });
 
   test('Should verify that the pet is gaining sleep', () => {
-    expect(newPet.restPet()).toEqual(11);
+    expect(newPet.restPet()).toEqual(10);
   });
 
   test('Should verify that the pet is gaining happiness', () => {
-    expect(newPet.playWithPet()).toEqual(11);
+    expect(newPet.playWithPet()).toEqual(10);
   });
 });
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -6,26 +6,40 @@ export class Pet {
   }
   
   feedPet() {
-    this.food += 1
-    return this.food;
+    if (this.food < 10) {
+      this.food += 1
+      return this.food;
+    }
   }
 
   playWithPet() {
-    this.happiness += 1
-    return this.happiness;
+    if (this.happiness < 10) {
+      this.happiness += 1
+      return this.happiness;
+    }
   }
 
   restPet() {
-    this.sleep += 1
-    return this.sleep;
+    if (this.sleep < 10) {
+      this.sleep += 1
+      return this.sleep;
+    }
   }
 
-  degredation(){
+  degredation() {
     setInterval(() => {
     this.food -= 1;
     this.sleep -= 1;
     this.happiness -= 1;
     return this.food;
-  }, 20000);
-}
+    }, 20000);
+  }
+
+  petStatus() {
+    if ((this.food <= 0) || (this.sleep <= 0) || (this. happiness <= 0)) {
+      alert("You suck at taking care of your animal");
+    } else if ((this.food <= 3) || (this.sleep <= 3) || (this. happiness <= 3)) {
+      alert("Your pet needs some attention!");
+    }
+  }
 }
