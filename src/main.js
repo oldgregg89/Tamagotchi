1ef10ff27deca660ceaa2649315bf7e0d5361293
Ian Gregg <iangregg188@gmail.com>
2020-06-29 11:39:51 -0700

    working userinterface, DOM, & CSS stylings.
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- src/main.js
+++ src/main.js
@@ -3,4 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 
-$(doc)
+$(document).ready(function() {
+  ("#name").submit(function() {
+    event.preventDefault();
+    
+  });
+
+  let newPet = new Pet(10, 10, 10);
+  newPet.degredation();
+
+  $("#feedPet").click(function() {
+    newPet.feedPet();
+  });
+
+  $("#playWithPet").click(function() {
+    newPet.playWithPet();
+  });
+
+  $("#restPet").click(function() {
+    newPet.restPet();
+  });
+});
